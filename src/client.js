20cbe41b093c7b9d580c0dda3c8140fa13d0936c
relmossaoui <redouan.elmossaoui2@gmail.com>
2019-11-27 10:52:24 +0100

    Generating and parsing WebSocket message frames

--- src/client.js
+++ src/client.js
@@ -1,16 +1,14 @@
-const ws = new WebSocket('ws://localhost:3210');
+const ws = new WebSocket('ws://localhost:3210', ["json", "xml"]);
 
 ws.addEventListener('open', () => {
-    // Send a message to the WebSocket server
-    ws.send('Hello!');
-});
-
+    const data = { message: 'Hello from the client!' }
+    const json = JSON.stringify(data);
+    ws.send(json);
+  });
 ws.addEventListener('message', event => {
-    // The `event` object is a typical DOM event object, and the message data sent
-    // by the server is stored in the `data` property
-    console.log('Received:', event.data);
+    const data = JSON.parse(event.data);
+    console.log(data);
 });
-
 
 
 
